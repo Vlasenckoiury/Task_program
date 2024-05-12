1873fa5f7a09897aad276e7ff79e744d0dd58f63
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-05-13 02:02:37 +0300

    Обновлен интерфейс

--- task.py
+++ task.py
@@ -1,17 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtGui import QIcon
 import psycopg2
 
-
-# conn = psycopg2.connect(
-#                 dbname="task",
-#                 user="postgres",
-#                 password="postgres",
-#                 host="localhost",
-#                 port="5432"
-#             )
 
 class Database:
     _instance = None
@@ -80,33 +72,57 @@ class ProductForm(QMainWindow):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.textEdit_name = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_name.setGeometry(QtCore.QRect(30, 80, 158, 39))
-        self.textEdit_name.setObjectName("textEdit_name")
+        self.name = QtWidgets.QLabel(self.centralwidget)
+        self.name.setGeometry(QtCore.QRect(30, 80, 161, 41))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        font.setBold(False)
+        font.setWeight(50)
+        self.name.setFont(font)
+        self.name.setObjectName("name")
 
         self.lineName = QtWidgets.QLineEdit(self.centralwidget)
         self.lineName.setGeometry(QtCore.QRect(210, 80, 231, 41))
+        self.lineName.setStyleSheet("font: 14pt MS Shell Dlg 2")
         self.lineName.setObjectName("lineName")
 
-        self.textEdit_price = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_price.setGeometry(QtCore.QRect(30, 150, 158, 39))
-        self.textEdit_price.setObjectName("textEdit_price")
+        self.price = QtWidgets.QLabel(self.centralwidget)
+        self.price.setGeometry(QtCore.QRect(30, 150, 161, 41))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        font.setBold(False)
+        font.setWeight(50)
+        self.price.setFont(font)
+        self.price.setObjectName("price")
 
         self.linePrice = QtWidgets.QLineEdit(self.centralwidget)
         self.linePrice.setGeometry(QtCore.QRect(210, 150, 231, 41))
+        self.linePrice.setStyleSheet("font: 14pt MS Shell Dlg 2")
         self.linePrice.setObjectName("linePrice")
 
-        self.textEdit_quantity = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_quantity.setGeometry(QtCore.QRect(30, 220, 158, 39))
-        self.textEdit_quantity.setObjectName("textEdit_quantity")
+        self.quantity = QtWidgets.QLabel(self.centralwidget)
+        self.quantity.setGeometry(QtCore.QRect(30, 220, 161, 41))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        font.setBold(False)
+        font.setWeight(50)
+        self.quantity.setFont(font)
+        self.quantity.setObjectName("quantity")
 
         self.spinBox1 = QtWidgets.QSpinBox(self.centralwidget)
         self.spinBox1.setGeometry(QtCore.QRect(210, 220, 231, 41))
+        self.spinBox1.setStyleSheet("font: 14pt MS Shell Dlg 2")
+        self.spinBox1.setMaximum(100000000)
         self.spinBox1.setObjectName("spinBox1")
 
-        self.textEdit_category = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_category.setGeometry(QtCore.QRect(30, 290, 158, 41))
-        self.textEdit_category.setObjectName("textEdit_category")
+        self.category = QtWidgets.QLabel(self.centralwidget)
+        self.category.setGeometry(QtCore.QRect(30, 290, 161, 41))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        font.setBold(False)
+        font.setWeight(50)
+        self.category.setFont(font)
+        self.category.setObjectName("category")
 
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox.setGeometry(QtCore.QRect(210, 290, 231, 41))
@@ -127,6 +143,12 @@ class ProductForm(QMainWindow):
         self.pushButtonDelete.setGeometry(QtCore.QRect(320, 400, 121, 51))
         self.pushButtonDelete.setStyleSheet("background-color:rgb(255, 0, 0)")
         self.pushButtonDelete.setObjectName("pushButtonDelete")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 350, 151, 41))
+        self.label.setMouseTracking(True)
+        self.label.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
+        self.label.setObjectName("label")
 
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
         self.tableWidget.setGeometry(QtCore.QRect(480, 80, 661, 481))
@@ -155,33 +177,15 @@ class ProductForm(QMainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("My Form", "My Form"))
         MainWindow.setWindowIcon(QIcon('icon.png'))
-        self.textEdit_quantity.setHtml(_translate("MainWindow",
-                                                  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                                  "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-                                                  "p, li { white-space: pre-wrap; }\n"
-                                                  "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-                                                  "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">Количество</span></p></body></html>"))
-        self.textEdit_category.setHtml(_translate("MainWindow",
-                                                  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                                  "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-                                                  "p, li { white-space: pre-wrap; }\n"
-                                                  "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-                                                  "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">Категория</span></p></body></html>"))
-        self.textEdit_price.setHtml(_translate("MainWindow",
-                                               "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                               "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-                                               "p, li { white-space: pre-wrap; }\n"
-                                               "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-                                               "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">Цена</span></p></body></html>"))
-        self.textEdit_name.setHtml(_translate("MainWindow",
-                                              "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                              "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-                                              "p, li { white-space: pre-wrap; }\n"
-                                              "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-                                              "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">Название</span></p></body></html>"))
+        self.name.setText(_translate("MainWindow", "Название"))
+        self.price.setText(_translate("MainWindow", "Цена"))
+        self.quantity.setText(_translate("MainWindow", "Количество"))
+        self.category.setText(_translate("MainWindow", "Категория"))
+
         self.pushButtonAdd.setText(_translate("MainWindow", "ADD"))
         self.pushButtonEdit.setText(_translate("MainWindow", "EDIT"))
         self.pushButtonDelete.setText(_translate("MainWindow", "DELETE"))
+
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "ID"))
         item = self.tableWidget.horizontalHeaderItem(1)
@@ -201,22 +205,6 @@ class ProductForm(QMainWindow):
         category_id = self.comboBox.itemData(category_index)
         db = Database.get_instance()
         db.save_product(name_val, price_val, quantity_val, category_id)
-
-    # def save_product(self):
-    #     name_val = self.lineName.text()
-    #     price_val = int(self.linePrice.text())
-    #     quantity_val = int(self.spinBox1.text())
-    #     category_val = int(self.comboBox.text())
-    #
-    #     try:
-    #         cursor = conn.cursor()
-    #         cursor.execute("INSERT INTO task (name, price, quantity, category_id) VALUES ( %s, %s, %s, %s)",
-    #                        (name_val, price_val, quantity_val, category_val))
-    #         conn.commit()
-    #         # conn.close()
-    #         print("Продукт успешно добавлен в базу данных.")
-    #     except (Exception, psycopg2.Error) as error:
-    #         print("Ошибка при работе с PostgreSQL:", error)
 
     def load_categories(self):
         db = Database.get_instance()
